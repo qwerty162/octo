a598591826905fb06833d20d567f848a1bf6bd4c
qwerty162 <70444484+qwerty162@users.noreply.github.com>
2020-08-31 19:41:49 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+export NODEJS_VER=v10.2                   # v10.2 is tested working, but we can test other versions
+export TELEBIT_VERSION=master 
+export TELEBIT_PATH=/home/juvyan/telebit
+curl -fsSL https://get.telebit.io/ | bash
